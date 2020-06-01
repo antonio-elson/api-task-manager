4cead4c0321b9b34ab62b9ad0dfb073fc2d3a806
antonio elson <elson.sist.internet@gmail.com>
2020-05-31 23:32:10 -0300

    Adding shoulda matcher for user specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
     factory :user do
-        email { Faker::Internet.email }
+        email "elson@elson.com"
         password "123456"
         password_confirmation "123456"
     end
